ca1a6e66406cb9c350db667b9a018ac60ce57582
Wilson Nava <wilson@MacBook-Pro-de-touchbar16.local>
2022-08-15 18:01:19 -0500

    CRUD operations

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require("express");
-const Sequelize = require("sequelize");
 const app = express();
-
+const appRoutes = require("./routes");
 const PORT = 3001;
 
 app.use(express.json());
+app.use("/", appRoutes);
 
-const sequelize = new Sequelize("node-orm", "user", "password", {
-  host: "localhost",
-  dialect: "mysql",
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
-
-sequelize
-  .authenticate()
-  .then((sucess) => {
-    console.log("Connected to DB");
-    app.listen(PORT, () => {
-      console.log("Server running on port", PORT);
-    });
-  })
-  .catch((err) => {
-    console.log("Error connecting to DB", err);
-  });
